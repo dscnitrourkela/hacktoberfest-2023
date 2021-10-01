3a85eadaad5aa55b38a50af54c59264d84289604
sonali sahu <87297567+sonali-012@users.noreply.github.com>
2021-10-01 20:52:27 +0530

    added sonali  (#5)
    * Update members.js
    
    * THIS IS SONALI
    
    Co-authored-by: Abhibhaw Asthana <39991296+abhibhaw@users.noreply.github.com>

--- src/members.js
+++ src/members.js
@@ -5,6 +5,11 @@ export default [
     linkedin: 'https://linkedin.com/in/riteshsp2000',
     portfolio: 'https://riteshpatil.dev',
   },
+  {
+    github: 'sonali-012',
+    twitter: 'http://twitter.com/@SonaliS28627844',
+    linkedin: 'https://www.linkedin.com/in/sonali-sahu-7510a3211',
+  },
 
   {
     github: 'karanShaw000',
