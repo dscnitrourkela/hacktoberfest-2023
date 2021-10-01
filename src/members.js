abb65c5ff0ff6b7d52ef5e8f7b3f1b7f9f3bec66
sonali-012 <sonali6918@gmail.com>
2021-10-01 20:23:11 +0530

    Update members.js

--- src/members.js
+++ src/members.js
@@ -5,4 +5,9 @@ export default [
     linkedin: 'https://linkedin.com/in/riteshsp2000',
     portfolio: 'https://riteshpatil.dev',
   },
+  {
+    github: 'sonali-012',
+    twitter: 'http://twitter.com/@SonaliS28627844',
+    linkedin: 'https://www.linkedin.com/in/sonali-sahu-7510a3211',
+  },
 ];
