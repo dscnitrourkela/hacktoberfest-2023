8bf62af83bcf6e6fb5a52b2cd8e97b8a4b9c3033
Subhasish <pandasonu589@gmail.com>
2021-10-01 20:22:14 +0530

    Add Subhasish as member

--- src/members.js
+++ src/members.js
@@ -5,4 +5,8 @@ export default [
     linkedin: 'https://linkedin.com/in/riteshsp2000',
     portfolio: 'https://riteshpatil.dev',
   },
+  {
+    github: 'sp7infinity',
+    linkedin: 'https://www.linkedin.com/in/subhasish-panda-3a677352/',
+  },
 ];
