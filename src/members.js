c60c4e360a0353e33a76cab61b7b8ceeb026e02e
sp7infinity <73926238+sp7infinity@users.noreply.github.com>
2021-10-01 20:43:17 +0530

    Add Subhasish as member (#2)
    Co-authored-by: Abhibhaw Asthana <39991296+abhibhaw@users.noreply.github.com>

--- src/members.js
+++ src/members.js
@@ -5,11 +5,13 @@ export default [
     linkedin: 'https://linkedin.com/in/riteshsp2000',
     portfolio: 'https://riteshpatil.dev',
   },
-
+  {
+    github: 'sp7infinity',
+    linkedin: 'https://www.linkedin.com/in/subhasish-panda-3a677352/',
+  },
   {
     github: 'nayakastha',
     twitter: 'https://twitter.com/AsthaNayak1',
     linkedin: 'https://linkedin.com/in/astha-nayak-an2001/',
-    
   },
 ];
