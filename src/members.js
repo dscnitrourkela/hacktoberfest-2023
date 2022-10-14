37245ed94df7857f844f651de65d6e59e176a8fe
Sashi6302 <115800604+Sashi6302@users.noreply.github.com>
2022-10-14 19:58:40 +0530

    feat: Add Sashi as a GDSC community member

--- src/members.js
+++ src/members.js
@@ -44,4 +44,53 @@ export default [
     github: 'ratnakar5938',
     twitter: 'https://twitter.com/ratnakar5938',
   },
+  {
+    github: 'riteshsp2000',
+    twitter: 'https://twitter.com/riteshsp2000',
+    linkedin: 'https://linkedin.com/in/riteshsp2000',
+    portfolio: 'https://riteshpatil.dev',
+  },
+  {
+    github: 'kesarivenkatesh',
+    linkedin: 'https://linkedin.com/in/kesarivenkatesh',
+  },
+  {
+    github: 'sonali-012',
+    twitter: 'http://twitter.com/@SonaliS28627844',
+    linkedin: 'https://www.linkedin.com/in/sonali-sahu-7510a3211',
+  },
+
+  {
+    github: 'karanShaw000',
+    twitter: 'https://twitter.com/KaranShaw123',
+  },
+  {
+    github: 'padmajabhol',
+  },
+  {
+    github: 'abhibhaw',
+    twitter: 'https://twitter.com/imabhibhaw',
+    portfolio: 'https://abhibhaw.co',
+  },
+  {
+    github: 'sp7infinity',
+    linkedin: 'https://www.linkedin.com/in/subhasish-panda-3a677352/',
+  },
+  {
+    github: 'nayakastha',
+    twitter: 'https://twitter.com/AsthaNayak1',
+    linkedin: 'https://linkedin.com/in/astha-nayak-an2001/',
+  },
+  {
+    github: 'pradhan-pk',
+    linkedin: 'https://www.linkedin.com/in/pradhan-pk/',
+  },
+  {
+    github: 'ratnakar5938',
+    twitter: 'https://twitter.com/ratnakar5938',
+  },
+    {
+    github: 'Sashi6302',
+    linkedin: 'https://www.linkedin.com/in/sashi-kumar-reddy-719181224/',
+  },
 ];
