40dfd8bdf06b7c8288fe61172c31d75b48170b96
prateek-10 <113173955+prateek-10@users.noreply.github.com>
2022-10-14 19:59:24 +0530

    feat: Add prateek as a GDSC community member

--- src/members.js
+++ src/members.js
@@ -44,4 +44,8 @@ export default [
     github: 'ratnakar5938',
     twitter: 'https://twitter.com/ratnakar5938',
   },
+  {
+    github: 'prateek-10',
+    linkedin: 'https://www.linkedin.com/in/prateek-kumar-nayak-72976822a',
+  },
 ];
